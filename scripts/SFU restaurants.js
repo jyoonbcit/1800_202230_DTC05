2f687605d48b1db5790173bca0a5b80340ec3466
Shuyi Liu <shuyiliu95@gmail.com>
2022-11-07 21:46:15 -0800

    Implement SFU restaurant page

---
+++ scripts/SFU restaurants.js
@@ -0,0 +1,52 @@
+//----------------------------------------
+//  Your web app's Firebase configuration
+//----------------------------------------
+var firebaseConfig = {
+    apiKey: "AIzaSyC31Ktxw_RjB0cKuw6Pg0dN0BPOHsIbMR0",
+    authDomain: "bcit-dtc05-project.firebaseapp.com",
+    projectId: "bcit-dtc05-project",
+    storageBucket: "bcit-dtc05-project.appspot.com",
+    messagingSenderId: "494917892160",
+    appId: "1:494917892160:web:ed4c9a297a09fe15179eb5"
+};
+
+//--------------------------------------------
+// initialize the Firebase app
+// initialize Firestore database if using it
+//--------------------------------------------
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+
+function displayCards(collection) {
+    let cardTemplate = document.getElementById("restaurantsCardTemplate");
+
+    db.collection(collection).get()
+        .then(snap => {
+            //var i = 1;  //if you want to use commented out section
+            snap.forEach(doc => { //iterate thru each doc
+                var title = doc.data().name;        // get value of the "name" key
+                var location = doc.data().location;   // get value of the "details" key
+                var hours = doc.data().hours;
+                var restaurantID = doc.data().code;
+                let newcard = cardTemplate.content.cloneNode(true);
+
+                //update title and text and image
+                newcard.querySelector('.card-title').innerHTML = title;
+                newcard.querySelector('.card-location').innerHTML = location;
+                newcard.querySelector('.card-hours').innerHTML = hours;
+                newcard.querySelector('.card-image').src = `../images/${restaurantID}.jpeg`; //Example: NV01.jpg
+
+                //give unique ids to all elements for future use
+                // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+                // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+                // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+
+                //attach to gallery
+                document.getElementById(collection + "-go-here").appendChild(newcard);
+                //i++;   //if you want to use commented out section
+            })
+        })
+}
+
+displayCards("SFU restaurants");
