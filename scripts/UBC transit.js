54b2e0e48653967c5e9bcec273f9dfb5a6b24e3c
hiem55 <100002888+hiem55@users.noreply.github.com>
2022-11-08 18:23:51 -0800

    add images

--- scripts/UBC transit.js
+++ scripts/UBC transit.js
@@ -28,14 +28,14 @@ function displayCards(collection) {
                 var title = doc.data().BusIDs;        // get value of the "BusIDs" key
                 var LocationName = doc.data().LocationName;   // get value of the "LocationName" key
                 var stop = doc.data().StopID;
-                var restaurantID = doc.data().code;
+                var transitID = doc.data().Code;
                 let newcard = cardTemplate.content.cloneNode(true);
 
                 //update title and text and image
                 newcard.querySelector('.card-title').innerHTML = title;
                 newcard.querySelector('.card-LocationName').innerHTML = LocationName;
                 newcard.querySelector('.card-stop').innerHTML = stop;
-                newcard.querySelector('.card-image').src = `../images/${restaurantID}.jpeg`; //Example: NV01.jpg
+                newcard.querySelector('.card-image').src = `../images/ubc_transit/${transitID}.jpeg`; //Example: NV01.jpg
 
                 //give unique ids to all elements for future use
                 // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
