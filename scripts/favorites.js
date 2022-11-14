8dfbe392a10b14d4d1653704cbafb6cd11e70311
hiem55 <100002888+hiem55@users.noreply.github.com>
2022-11-13 21:06:20 -0800

    work in progress add favorites

---
+++ scripts/favorites.js
@@ -0,0 +1,42 @@
+function createSomeLots() {
+    for (var i = 1; i < 5; i++) {
+        db.collection("lots").doc("lot" + i).set({
+            details: "blah blah lot" + i,
+            status: "empty",
+            lastupdate: firebase.firestore.FieldValue.serverTimestamp()
+        })
+    }
+}
+
+function displayLots() {
+    db.collection("lots").get().then(snap => {
+        snap.forEach(doc => {
+            lotname = doc.id;
+            status = doc.data().status;
+            let lotCard = document.getElementById("lotcard").content.cloneNode(true);
+            lotCard.querySelector('.stuff').innerHTML = lotname + " status: " + status;
+            lotCard.querySelector('.fave').onclick = () => setFave(lotname);
+            document.getElementById("lots-go-here").appendChild(lotCard);
+        })
+    })
+}
+displayLots();
+
+function setFave(lotid) {
+    firebase.auth().onAuthStateChanged(function (user) {
+        db.collection("users").doc(user.uid).set({
+            favourites: firebase.firestore.FieldValue.arrayUnion(lotid)
+        }, { merge: true });
+    })
+}
+
+function displayFaveLots() {
+    firebase.auth().onAuthStateChanged(function (user) {
+        db.collection("users").doc(user.uid).get().then(doc => {
+            faves = doc.data().favourites;
+            console.log(faves)
+            displayLots(faves)
+        })
+    })
+}
+displayFaveLots();
