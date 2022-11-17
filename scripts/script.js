a2920f9eb1fb8f3ad827fb209118c9612225ee81
hiem55 <100002888+hiem55@users.noreply.github.com>
2022-11-17 11:22:15 -0800

    small change

--- scripts/script.js
+++
@@ -1,9 +0,0 @@
-$("logoutBtn").click(function () {
-    firebase.auth().signOut().then(function () {
-        // Sign-out successful.
-        console.log("User signed out");
-    }).catch(function (error) {
-        // An error happened.
-        console.log("Error signing out");
-    });
-});
