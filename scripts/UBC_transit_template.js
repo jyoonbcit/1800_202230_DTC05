87a3f3d088786ccd13c568c30d07ee088e8fbace
hiem55 <100002888+hiem55@users.noreply.github.com>
2022-11-22 00:42:26 -0800

    delete redundant pages and add authen function

--- scripts/UBC_transit_template.js
+++ scripts/UBC_transit_template.js
@@ -54,3 +54,29 @@ function displayCards() {
 }
 
 displayCards("Stops");
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        var email = user.email;
+        console.log(email, "is signed in");
+        $("#loginBtn").hide();
+        // ...
+    } else {
+        console.log("No user is signed in");
+        $("#logoutBtn").hide();
+        // User is signed out
+        // ...
+    }
+});
+
+$("logoutBtn").click(function () {
+    firebase.auth().signOut().then(function () {
+        // Sign-out successful.
+        console.log("User signed out");
+    }).catch(function (error) {
+        // An error happened.
+        console.log("Error signing out");
+    });
+});
