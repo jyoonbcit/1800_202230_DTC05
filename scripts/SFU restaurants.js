7b60da0e2c4b083cde47fd2d86ac422b06a0fdff
hiem55 <100002888+hiem55@users.noreply.github.com>
2022-11-23 13:39:09 -0800

    update firebase script into different file

--- scripts/SFU restaurants.js
+++ scripts/SFU restaurants.js
@@ -1,21 +1,3 @@
-//----------------------------------------
-//  Your web app's Firebase configuration
-//----------------------------------------
-var firebaseConfig = {
-    apiKey: "AIzaSyC31Ktxw_RjB0cKuw6Pg0dN0BPOHsIbMR0",
-    authDomain: "bcit-dtc05-project.firebaseapp.com",
-    projectId: "bcit-dtc05-project",
-    storageBucket: "bcit-dtc05-project.appspot.com",
-    messagingSenderId: "494917892160",
-    appId: "1:494917892160:web:4a4972c9f10060c8c587a5"
-};
-
-//--------------------------------------------
-// initialize the Firebase app
-// initialize Firestore database if using it
-//--------------------------------------------
-const app = firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
 
 
 function displayCards(collection) {
